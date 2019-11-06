896cfdc92c09e74f4145d6edc8fba3c5c8500dcf
sai <sindhura.shetty@yahoo.com>
2019-11-06 15:48:47 +0530

    sai

---
+++ HibernateEx/src/main/java/com/lti/hibernateex/HibernateEx/App.java
@@ -0,0 +1,31 @@
+package com.lti.hibernateex.HibernateEx;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+  Configuration configuration = new Configuration() .configure();
+  StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+  SessionFactory factory = configuration.buildSessionFactory(builder.build());
+  Session session = factory.openSession();
+  Employee employee = new Employee();
+  employee.setEmpName("sai");
+  employee.setBranch("chennai");
+  session.beginTransaction();
+  session.save(employee);
+  session.getTransaction().commit();
+  
+		  
+	  
+    }
+}
---
+++ HibernateEx/src/main/java/com/lti/hibernateex/HibernateEx/Employee.java
@@ -0,0 +1,26 @@
+package com.lti.hibernateex.HibernateEx;
+
+public class Employee {
+	private int id;
+	private String empName;
+	private String branch;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+}
